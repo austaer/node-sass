9c00e6ba1da43aea87e533f5ff04a2298a2c4c50
bob <bob@novellink.com>
2017-09-27 15:54:17 +0800

    add command

---
+++ node-sass.bat
@@ -0,0 +1,4 @@
+npm install gulp -g
+npm link gulp
+echo {  "name": "gulp-sass",  "version": "1.0.0",  "description": "",  "main": "index.js",  "scripts": {    "test": "echo \"Error: no test specified\" && exit 1"  },  "author": "",  "license": "ISC",  "dependencies": {    "gulp": "^3.9.1",    "gulp-sass": "^2.3.2"  }} > package.json
+PAUSE
